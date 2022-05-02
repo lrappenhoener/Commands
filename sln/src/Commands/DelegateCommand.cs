8ee5e03f698c54b880a9748d31480e1b93d8d2d3
lr <lrmail>
2022-05-02 20:51:24 +0200

    Fix some Nullable warnings

--- sln/src/Commands/DelegateCommand.cs
+++ sln/src/Commands/DelegateCommand.cs
@@ -5,10 +5,10 @@ namespace PCC.Libraries.Commands;
 
 public class DelegateCommand : ICommand
 {
-    private readonly Action<object> _execute;
-    private readonly Func<object, bool> _canExecute;
+    private readonly Action<object?> _execute;
+    private readonly Func<object?, bool> _canExecute;
 
-    public DelegateCommand(Action<object> execute, Func<object,bool> canExecute)
+    public DelegateCommand(Action<object?> execute, Func<object?,bool> canExecute)
     {
         _execute = execute;
         _canExecute = canExecute;
