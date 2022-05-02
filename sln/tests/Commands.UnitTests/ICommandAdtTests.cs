a620fb21797cd9939a1f039032e2ee20dde4f970
lr <lrmail>
2022-05-02 20:12:46 +0200

    Introduce ICommandAdtTests

---
+++ sln/tests/Commands.UnitTests/ICommandAdtTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Windows.Input;
+using FluentAssertions;
+using Xunit;
+
+namespace PCC.Libraries.Commands.UnitTests;
+
+public abstract class ICommandAdtTests
+{
+    protected abstract ICommand CreateSut(Action<object> execute, Func<object, bool> canExecute);
+    protected abstract ICommand CreateSut(Func<object, bool> canExecute);
+    
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void CanExecute_Returns_Correct_Value(bool expected)
+    {
+        var sut = CreateSut((state) => expected);
+
+        sut.CanExecute(null).Should().Be(expected);
+    }
+    
+    /*event EventHandler? CanExecuteChanged;
+
+    bool CanExecute(object? parameter);
+
+    void Execute(object? parameter);*/
+}
